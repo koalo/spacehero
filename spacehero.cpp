f30a8af1a99c7869b47aae1922bd8698a83338fe
Arne Wichmann <arnew@rasentrimmer.org>
2009-02-08 18:00:22 +0100

    skip intro if level is specified

--- spacehero.cpp
+++ spacehero.cpp
@@ -14,59 +14,27 @@ int main(int argc, char *argv[])
 	std::string levels("./level/");
 	GLdisplay display;
 
-	intro(display);
-	std::cerr << "intro done" << std::endl;
 	std::cerr << "argc: " << argc << std::endl;
 
-	if(argc<=1)
-	{
-	std::cerr << "leveldir: "<< levels << std::endl;
-		if (is_directory(levels) )
-		{
-	std::cerr << "level dir found" << std::endl;
+	if(argc<=1) {
+		std::cerr << "leveldir: "<< levels << std::endl;
+		if (is_directory(levels) ) {
+			std::cerr << "level dir found" << std::endl;
 
-			for (directory_iterator itr(levels); itr != directory_iterator(); ++itr) 
-			{
+			intro(display);
+			std::cerr << "intro done" << std::endl;
+
+			for (directory_iterator itr(levels); itr != directory_iterator(); ++itr) {
 				std::cerr << "trying to load level: " << itr->path() << std::endl;
 				std::ifstream level(itr->path().string().c_str());
 				if(level) display.startRound(level);
 			}
 		}
-	}
-	else {
+	} else {
 		std::ifstream level(argv[1]);
 		display.startRound(level);
 
 	}
-	/*
-	//char levelString[50];
-	int rlevel = -1, altrlevel;
-
-
-	srand((unsigned int) time(NULL));
-
-	//  SDL_ShowCursor(SDL_DISABLE);
-	//  intro(&display);
-	//SDL_ShowCursor(SDL_ENABLE);
-
-	do
-	{
-	if(argc == 1)
-	{
-	altrlevel = rlevel;
-	do
-	{
-	rlevel = (int)((rand() / (RAND_MAX + 1.0))*LEVEL)+1;
-	} while(rlevel == altrlevel);
-
-	sprintf(levelString,"level%i",rlevel);
-	} else {
-	strcpy(levelString,argv[1]);
-	}
-
-	startRound(&display,levelString);
-	} while(rlevel != -1 && !display.state.exit);
-	*/
 
 	return 0;
 }
