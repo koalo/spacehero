6f7dd443ab8e8b1db54af67508c0f3b719f06214
Arne Wichmann <arnew@rasentrimmer.org>
2009-02-19 18:48:25 +0100

    new level format used in output

--- output.cpp
+++ output.cpp
@@ -1,43 +1,43 @@
 
 #include "Universe.h"
 
-
-
 std::ostream& operator<< (std::ostream &o, const SkyObject &g) {
-  o << "SkyObject: " << std::endl;
-  o << "(x,y,z): " << g.x << " " << g.y << " " << g.z << std::endl;
+//  o << "SkyObject: " << std::endl;
+  o << "P " << g.x << " " << g.y << " " << g.z << std::endl;
   return o;
 }
 
 std::ostream& operator<< (std::ostream &o, const SkyMass &g) {
-  o << "SkyMass: " << std::endl;
-  o << "mass: " << g.mass << std::endl;
+  //o << "SkyMass: " << std::endl;
   //o << "(fx,fy,fz): " << g.fx << " " << g.fy << " " << g.fz << std::endl;
-  o << "(vx,vy,vz)" << g.vx << " " << g.vy << " " << g.vz << std::endl;
   o << static_cast<SkyObject>(g) << std::endl;
+  o << "V " << g.vx << " " << g.vy << " " << g.vz << std::endl;
+  o << "W " << g.mass << std::endl;
   return o;
 }
 std::ostream& operator<< (std::ostream &o, const Goal &g) {
-  o << "Goal: " << std::endl;
-  o << "radius: " << g.radius << std::endl;
+  o << "D " << std::endl;
   o << static_cast<SkyObject>(g) << std::endl;
+  o << "R " << g.radius << std::endl;
   return o;
 }
 std::ostream& operator<< (std::ostream &o, const Blackhole &g) {
-  o << "Blackhole: " << std::endl;
+  o << "H" << std::endl;
   o << static_cast<SkyMass>(g) << std::endl;
   return o;
 }
 std::ostream& operator<< (std::ostream &o, const Star &g) {
-  o << "Star: " << std::endl;
+  o << "S" << std::endl;
   o << static_cast<SkyMass>(g) << std::endl;
   return o;
 }
 std::ostream& operator<< (std::ostream &o, const Galaxy &g) {
-  o << "Galaxy: " << std::endl;
+  o << "G" << std::endl;
+  o << g.master?" M ":"" << std::endl;
   o << static_cast<SkyMass>(g) << std::endl;
   return o;
 }
+
 std::ostream& operator<< (std::ostream &o, const Level &l) {
   o << l.goal << std::endl;
   copy(l.holes.begin(),l.holes.end(),std::ostream_iterator<Blackhole>(o));
